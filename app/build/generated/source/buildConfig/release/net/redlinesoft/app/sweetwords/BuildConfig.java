159b176e57bd0e8711472582fa4301847f8c0f1e
Anuchit Chalothorn <anoochit@gmail.com>
2014-08-10 08:07:26 +0700

    Initial commit

---
+++ app/build/generated/source/buildConfig/release/net/redlinesoft/app/sweetwords/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package net.redlinesoft.app.sweetwords;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String PACKAGE_NAME = "net.redlinesoft.app.sweetwords";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "";
+}
---
+++ app/src/main/java/net/redlinesoft/app/sweetwords/CustomSimpleAdapter.java
@@ -0,0 +1,27 @@
+package net.redlinesoft.app.sweetwords;
+
+import java.util.List;
+import java.util.Map;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SimpleAdapter;
+
+public class CustomSimpleAdapter extends SimpleAdapter {
+
+	public CustomSimpleAdapter(Context context,
+			List<? extends Map<String, ?>> data, int resource, String[] from,
+			int[] to) {
+		super(context, data, resource, from, to);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		return super.getView(position, convertView, parent);
+	}
+
+	
+}
